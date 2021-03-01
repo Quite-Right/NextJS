f903491587624e84e73705f915a8891f03de5053
Novogrudskiy Dmitry <novogrudskiy.dv@gmail.com>
2021-03-01 21:28:37 +0300

    Assets, Metadata, and CSS

--- nextjs-blog/pages/posts/second-post.js
+++ nextjs-blog/pages/posts/second-post.js
@@ -1,20 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head";
 
-export default function FirstPost() {
+export default function SecondPost() {
   return (
     <>
       <Head>
-        <title>First post</title>
+        <title>Second post</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>First Post</h1>
+      <h1>Second Post</h1>
       <h2>
         Back to the <Link href="/">main page</Link>
       </h2>
+      <Image height="144" width="144" src="/images/react-image.png" alt="react" />
       <article>
-        Note: to style Link component using className, etc. use inner component
-        (prefered a tag)
+
       </article>
     </>
   );
