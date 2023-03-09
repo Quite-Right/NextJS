9ded0b44000574afa07d46c669fb6febfb70e1f2
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-03-09 20:59:24 +0300

    client side routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
-const Home = () => <h1>Home page</h1>;
+import Link from "next/link";
+
+const Home = () => <>
+    <h1>Home page</h1>
+    <Link href="./about">
+        <div>
+            About
+        </div>
+    </Link>
+    <Link href="./product">
+        <div>
+            Product
+        </div>
+    </Link>
+    <Link href="./docs">
+        <div>
+            Docs
+        </div>
+    </Link>
+</>;
 
 export default Home;
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 const ProductList = () => <>
-    <h2>
-        Product 1
-    </h2>
-    <h2>
-        Product 2
-    </h2>
-    <h2>
-        Product 3
-    </h2>
+    <Link href="/product/1">
+        <h2>
+            Product 1
+        </h2>
+    </Link>
+    <Link href="/product/2">
+        <h2>
+            Product 2
+        </h2>
+    </Link>
+    <Link href="/product/3" replace>
+        <h2>
+            Product 3
+        </h2>
+    </Link>
 </>
 
 export default ProductList;
