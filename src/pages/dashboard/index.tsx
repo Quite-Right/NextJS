f3f9857e143f5d875dd490572bbbe1990fb1a021
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-03-12 02:37:25 +0300

    client side fetching

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+
+export default function Dashboard() {
+ const [request, setRequest] = useState<{
+    data: null | Record<string, number>,
+    fetching: boolean
+ }>({
+    data: null,
+    fetching: true
+ });
+
+ const {data, fetching} = request;
+
+ useEffect(() => {
+    (async () => {
+        const res = await fetch("http://localhost:4000/dashboard")
+        const data = await res.json();
+        setRequest({data, fetching: false});
+    })()
+ }, []);
+
+ if (fetching) return <h1>Loading...</h1>
+ return <>
+    <h1>
+        Dashboard
+    </h1>
+    <div>
+        posts: {data.posts}
+    </div>
+    <div>
+        posts: {data.likes}
+    </div>
+    <div>
+        posts: {data.followers}
+    </div>
+    <div>
+        posts: {data.following}
+    </div>
+ </>
+}
