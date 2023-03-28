1c7d97596f6badf30c48e06d5b0157a39b4f85aa
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-03-28 23:03:42 +0300

    global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+// ADD GLOBAL STYLING due to convention in _app file
+// you can add anything like material ui or bootstrap here
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
