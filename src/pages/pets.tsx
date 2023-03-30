7d2c78a01d957e809d1ee722bad56ed5e00e3ad0
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-03-31 01:10:43 +0300

    Image - nest

---
+++ src/pages/pets.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import imgPlaceholder from '../../public/1.jpg'
+
+export default function PetsPage() {
+    return <>
+        <h1>Pets page</h1>
+        <Image placeholder="blur" src={imgPlaceholder} alt='pet' width={280} height={420} />
+        <div>
+            {['1', '2', '3', '4', '5'].map(petId => <div key={petId}>
+                <Image src={`/${petId}.jpg`} alt='pet' width={280} height={420} />
+            </div>)}
+        </div>
+    </>
+}
