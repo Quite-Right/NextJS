e27aafc0f9c61ec194fdb5ec6d8b501afefe0aae
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-04-08 19:29:04 +0300

    added server side auth and redirect

--- src/pages/api/blog/index.ts
+++ src/pages/api/blog/index.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-    return res.status(200).json({name: 'Blog API route'});
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    const session = await getSession({req});
+    if (!session) {
+        res.status(401).json('Unauthorized');
+    } else {
+        res.status(200).json({name: 'Blog API route'});
+    }
 }
 
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,13 @@
 import styles from '@/styles/Blog.module.scss'
+import { getSession } from 'next-auth/react';
 
 interface IBlogProps {
     DB_USER: string;
     DB_PASSWORD: string;
+    data: string;
 }
 
-const Blog = ({DB_USER, DB_PASSWORD}: IBlogProps) => <>
+const Blog = ({DB_USER, DB_PASSWORD, data}: IBlogProps) => <>
     <h1 className={styles.header}>Blog page</h1>
     <div>
         <span>
@@ -25,15 +27,26 @@ const Blog = ({DB_USER, DB_PASSWORD}: IBlogProps) => <>
         </span>
         {process.env.NEXT_PUBLIC_DATA}
     </div>
+    <div>
+        DATA: {data}
+    </div>
 </>;
 
-export const getServerSideProps = () => {
+export async function getServerSideProps(context) {
     const {DB_USER, DB_PASSWORD} = process.env;
-    console.log(process.env)
+    const session = await getSession(context);
+    console.log({session})
+    if (!session) return {
+        redirect: {
+            destination: '/api/auth/signin?callbackUrl=/blog',
+            permament: false
+        }
+    }
     return {
         props: {
             DB_USER,
-            DB_PASSWORD
+            DB_PASSWORD,
+            data: session ? 'List of personalized blogs' : 'Default blogs'
         }
     }
 }
